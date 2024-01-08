2abcad4ff2b410d91f57f362e8b6c6233c94218a
Stas Ovechenko <user975345@gmail.com>
2024-01-08 23:18:04 +0200

    SegWay

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('btn').addEventListener('click', onClickBtn);
 function onClickBtn() {
     document.getElementById('menu').classList.toggle('visible');
     document.getElementById('btn').classList.toggle('visible');
-    document.getElementById('body').classList.toggle('lock')
+    document.getElementById('body').classList.toggle('block');
 }
